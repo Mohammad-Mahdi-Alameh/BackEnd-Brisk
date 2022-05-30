c118f216cd906660819e8bb1f41095636507510b
Mohammad-Mahdi-Alameh <mmwalameh@gmail.com>
2022-05-30 14:49:41 +0300

    Creating get reviews and rates api

--- apis/get_restaurants.php
+++ apis/get_restaurants.php
@@ -1,13 +1,21 @@
 <?php
+
 include("connection.php");
+
 $query = $mysqli->prepare("SELECT * from restaurants");
+
 $query->execute();
+
 $array = $query->get_result();
+
 $response = [];
-while($todo = $array->fetch_assoc()){
-    $response[] = $todo;
+
+while($restaurant = $array->fetch_assoc()){
+    $response[] = $restaurant;
 }
 //$json = json_encode($response);
 //echo $json;
+
 echo json_encode($response);
+
 ?>
