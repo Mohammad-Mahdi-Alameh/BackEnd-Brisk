329da64c45e51c2c68e834885450e023f18de88b
Mohammad-Mahdi-Alameh <mmwalameh@gmail.com>
2022-05-30 14:41:38 +0300

    Creating api to get all users

---
+++ apis/get_users.php
@@ -0,0 +1,22 @@
+<?php
+
+include("connection.php");
+
+
+$query = $mysqli->prepare("SELECT * from users");
+
+$query->execute();
+
+$array = $query->get_result();
+
+$response = [];
+
+while($user = $array->fetch_assoc()){
+    $response[] = $user;
+}
+
+
+echo json_encode($response);
+
+
+?>
