44c0075e66607777a8c68b547f1af1fb33cb6443
Mohammad-Mahdi-Alameh <mmwalameh@gmail.com>
2022-05-30 11:18:56 +0300

    Creating login api

--- apis/login.php
+++ apis/login.php
@@ -1,8 +1,13 @@
 <?php
+
 include("connection.php");
+
 $username = $_POST["username"];
+
 $password =  $_POST["password"];
+
 $response = [];
+
 $sql = "SELECT * FROM users WHERE username='$username' AND password='$password'";
 
         $result = mysqli_query($mysqli, $sql);
@@ -10,20 +15,22 @@ $sql = "SELECT * FROM users WHERE username='$username' AND password='$password'"
         if (mysqli_num_rows($result) === 1) {
 
           $row = mysqli_fetch_assoc($result);
+
           $response["response"] = "Logged in";
          
            $response["response"]=$row;
-          //$response["user_id"] = $row['user_id']; 
+
+           //$response["user_id"] = $row['user_id']; 
          // $response["is_admin"] = $row['is_admin']; 
 
         //  $_SESSION['id']=$id;
-         
-  
+           
 }
 
-        else{
+        else
+        
             $response["response"] = "UserNotFound";
-        }
+        
         $json = json_encode($response);
         echo $json;
 ?>
